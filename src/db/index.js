0d8759906941ba979cd9d46d5448c8d7121db5c8
ishan <ishansinha2008@gmail.com>
2026-08-02 20:51:02 +0530

    fixed database connection errors

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,17 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI
+
+        if (!mongoUri) {
+            throw new Error('MONGODB_URI or MONGO_URI environment variable is not defined')
+        }
+
+        const connectionInstance = await mongoose.connect(`${mongoUri}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+        return connectionInstance
     } catch (error) {
-        console.log("MONGODB connection FAILED", error)
+        console.error('MONGODB connection FAILED', error)
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,65 +1,19 @@
-// require('dotenv').config({path: './env'})
-
 import dotenv from 'dotenv'
-
-import mongoose from 'mongoose'
-import { DB_NAME } from './constants.js'
 import connectDB from './db/index.js'
+import { app } from './app.js'
 
-dotenv.config({
-    path: './.env'
-})
+dotenv.config({ path: './.env' })
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-        console.log(`The server is up and running at port: ${process.env.PORT}`)
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!!", err)
-})
+const startServer = async () => {
+    try {
+        await connectDB()
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`The server is up and running at port ${process.env.PORT || 5000}`)
+        })
+    } catch (error) {
+        console.error('Failed to start the server:', error)
+        process.exit(1)
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Initializing databse on index - Approach - 1
-// import express from 'express' 
-
-// const app = express()
-
-// (async () => {
-//     try {
-//         await mongoose.connect('${process.env.MONGO_URI}/${DB_NAME}')
-//         app.on("error", (error) => {
-//             console.log("Application not able to talk to databse")
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log('App is listening on port ${process.env.PORT}')
-//         })
-
-//     } catch (error) {
-//         console.error("Error: ", error)
-//         throw err
-//     }
-// })()
+startServer()
