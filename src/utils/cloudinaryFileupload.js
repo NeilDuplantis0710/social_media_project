daef26e5e82126fd86df115330fe678ab7822e48
ishan <ishansinha2008@gmail.com>
2026-08-03 16:58:37 +0530

    Fixed cloudinary config

--- src/utils/cloudinaryFileupload.js
+++ src/utils/cloudinaryFileupload.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from "cloudinary" //v2 has a random name, in this case cloudinary, so we can use it in our code.
 import fs from "fs"
 
-import { v2 as cloudinary } from 'cloudinary';
 
 
 // Configuration
