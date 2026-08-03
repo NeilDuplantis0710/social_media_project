4430bb0aa13e43d191064a410de734ce424fc47e
ishan <ishansinha2008@gmail.com>
2026-08-03 19:58:35 +0530

    Fixed AsyncHandler

--- src/utils/AyncHandler.js
+++ src/utils/AyncHandler.js
@@ -4,13 +4,13 @@
 // export { asyncHandler }
 
 
-const asyncHandler = (fn) => async(error,req,res,next) => {
-    try{
-        await fn(req,res,next)
-    }catch(error){
-        res.status(err.code || 500).json({
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
             success: false,
-            message: err.message
+            message: error.message
         })
     }
 }
