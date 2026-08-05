ed9f886e680737031aef53fff38bbc730b851b3b
ishan <ishansinha2008@gmail.com>
2026-08-05 21:32:45 +0530

    Made a authorization middleware for clearing the cookies and refreshtokens and accesstokens

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/AyncHandler.js"
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js"
+
+export default verifyJwt = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Autherization")?.replace("Bearer ", "") //Access token is optional within cookies or user sends a header in request, and taking only the token value from "Bearer: <token>".
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized token")
+        }
+
+        const decodedToken = jwt.verify(token, proccess.env.ACCESS_TOKEN_SECRET) //Verify our token from the Access token stored in the .env file.
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken") //If we have decodedToken, it is wrapped unoptionally.
+
+        if (!User) {
+            throw new ApiError(401, "Invalid Access Token")
+        }
+
+        req.user = user //sending the access to the user in another variable named user going to the request.
+        next() // The middleware passes the work throug next().
+    } catch (error) {
+        throw new ApiError(401, "Invalid Access Token")
+    }
+
+})
