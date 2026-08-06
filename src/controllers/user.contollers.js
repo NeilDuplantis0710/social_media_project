f6e7ce721fc34cf8708e7a0c5f7734e255cb741a
ishan <ishansinha2008@gmail.com>
2026-08-06 18:11:31 +0530

    fixed the login  validation logic

--- src/controllers/user.contollers.js
+++ src/controllers/user.contollers.js
@@ -119,8 +119,9 @@ const loginUser = asyncHandler(async (req, res) => {
     // Get the data from req body.
 
     const { username, email, password } = req.body
+    console.log(email)
 
-    if (!username || !email) {
+    if (!username && !email) {
         throw new ApiError(400, "Username or email is required.")
     }
     // Login on the basis of username and email and find the username in database.
